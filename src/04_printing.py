ccac1f4f24102835ecb408f171b5c49e228a3ceb
Paul Abernathy <paul.abernathy7@gmail.com>
2020-07-11 17:34:55 -0400

    printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -3,17 +3,18 @@ Python provides a number of ways to perform printing. Research
 how to print using the printf operator, the `format` string 
 method, and by using f-strings.
 """
-
 x = 10
 y = 2.24552
 z = "I like turtles!"
-
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
-
+# for reference https://www.python-course.eu/python3_formatted_output.php
+print('\nPrint Operator %:')
+print('x is% 2d, y is% 2.2f, z is "% s"' % (x, y, z), '\n')
 # Use the 'format' string method to print the same thing
-
-
+print('Format String Method:')
+print('x is {0}, y is {1:.2f}, z is "{2}"'.format(x, y, z), '\n')
 # Finally, print the same thing using an f-string
+print('F-String:')
+print(f'x is {x}, y is {round(y, 2)}, z is "{z}"', '\n')
