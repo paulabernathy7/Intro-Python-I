a23589c8d369c982548dc1a2e83c712d0a6f40bc
Paul Abernathy <paul.abernathy7@gmail.com>
2020-07-12 15:46:33 -0400

    modules

--- src/03_modules.py
+++ src/03_modules.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import platform
+
 """
 In this exercise, you'll be playing around with the sys module,
 which allows you to access many system specific variables and
@@ -10,22 +14,35 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+if len(sys.argv) > 1:
+    print(sys.argv[1])
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(os.name)
+print(platform.system())
+print(platform.release())
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
 
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("PID of the current process is:")
 
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("CWD is:")
+
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("user logged in")
+print(os.getlogin())
